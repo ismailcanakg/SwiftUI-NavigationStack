964354ecca1581d8711297d907aa48f63e016290
İsmail Can Akgün <ismailcanakg1@gmail.com>
2024-05-17 17:54:08 +0300

    Initial Commit

---
+++ SwiftUI-NavigationStack/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI-NavigationStack
+//
+//  Created by İsmail Can Akgün on 17.05.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI-NavigationStack/SwiftUI_NavigationStackApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_NavigationStackApp.swift
+//  SwiftUI-NavigationStack
+//
+//  Created by İsmail Can Akgün on 17.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_NavigationStackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
